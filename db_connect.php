a36422c2f631a9323dc1129c5b00e83eeba8ec17
AustinMuriithiSchool <austin.muriithi@strathmore.edu>
2024-10-02 18:39:34 +0300

    d

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "nourishnet";
+$username = "root";  // Your MySQL root username
+$password = "";  // Your root password
+$dbname = "CAT1";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,6 +11,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
-
+echo "Connected successfully";
+?>

63d8ab1b25623f69876741d86e1fa59e97d9efea
AustinMuriithiSchool <austin.muriithi@strathmore.edu>
2024-10-02 22:49:26 +0300

    dd

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "102.37.157.159";
+$servername = "127.0.0.1";
 $username = "root";  // Your MySQL root username
 $password = "@U5tiniscool";  // Your root password
 $dbname = "CAT1";
